7ff594fb201e4deb43303f750a16e3cba01e681b
matthewsmkim <forme3328@naver.com>
2022-11-04 17:48:19 +0900

    First commit

---
+++ src/main/java/hello/springcoreproject/SpringCoreProjectApplication.java
@@ -0,0 +1,13 @@
+package hello.springcoreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springcoreproject/SpringCoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springcoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
